a215c35b54617314ac7ff6c841e417e07c1379a6
KDBismael <ismaelkondombo1@gmail.com>
2023-12-01 12:36:04 +0000

    first commit

---
+++ lib/app.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "ScanX",
+      debugShowCheckedModeBanner: false,
+      theme: appTheme,
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(color: Theme.of(context).colorScheme.primary),
+        child: Center(
+          child: Text(
+            'Here we go',
+            style: TextStyle(color: Theme.of(context).colorScheme.tertiary),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+ThemeData appTheme = ThemeData(
+  brightness: Brightness.light,
+  colorScheme: const ColorScheme(
+    brightness: Brightness.light,
+    primary: Color(0xFF0ABAB5),
+    onPrimary: Color.fromARGB(255, 13, 240, 232),
+    secondary: Color(0xFFF2F5F5),
+    onSecondary: Color.fromARGB(255, 242, 245, 245),
+    tertiary: Color(0xFFF1D427),
+    onTertiary: Color(0xFFF1D427),
+    error: Color(0xFFF32424),
+    onError: Color(0xFFF32424),
+    background: Color(0xFFF1F2F3),
+    onBackground: Color(0xFFFFFFFF),
+    surface: Color(0xFFF2F5F5),
+    onSurface: Color(0xFFF2F5F5),
+  ),
+  // textTheme: TextTheme(),
+);
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:scanx/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
