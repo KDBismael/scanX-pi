4575c35d7d0fa30cbcfef94a9ad6f142f7f43f5d
KDBismael <ismaelkondombo1@gmail.com>
2024-01-26 12:14:49 +0000

    profile Ui

--- lib/features/auth/presentation/screens/profile_screen.dart
+++ lib/features/auth/presentation/screens/profile_screen.dart
@@ -1,3 +1,4 @@
+import 'package:datepicker_dropdown/datepicker_dropdown.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
@@ -10,6 +11,11 @@ class ProfileScreen extends StatelessWidget {
     final screenSize = MediaQuery.of(context).size;
     List<XFile>? mediaFileList;
     final picker = ImagePicker();
+    DateTime dateNow = DateTime(1995, 12, 31);
+
+    int selectedDay = dateNow.day;
+    int selectedMonth = dateNow.month;
+    int selectedYear = dateNow.year;
 
     return Scaffold(
       body: SafeArea(
@@ -35,8 +41,9 @@ class ProfileScreen extends StatelessWidget {
                 child: SingleChildScrollView(
                   child: SizedBox(
                     width: screenSize.width,
-                    // height: screenSize.height,
+                    height: screenSize.height * 0.827,
                     child: Column(
+                      mainAxisSize: MainAxisSize.min,
                       children: [
                         Stack(
                           children: [
@@ -91,7 +98,178 @@ class ProfileScreen extends StatelessWidget {
                           "@gnaorekouame",
                           style: TextStyle(
                               fontSize: 16, fontWeight: FontWeight.w300),
-                        )
+                        ),
+                        SizedBox(height: screenSize.height * 0.04),
+                        Form(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Padding(
+                                padding: EdgeInsets.only(bottom: 5),
+                                child: Text.rich(
+                                  textAlign: TextAlign.start,
+                                  TextSpan(
+                                    children: [
+                                      WidgetSpan(
+                                        child: Text(
+                                          'Nom Complet',
+                                          style: TextStyle(
+                                              fontSize: 16,
+                                              fontWeight: FontWeight.w300,
+                                              color: Colors.grey),
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              TextFormField(
+                                initialValue: "Gnaore Kouame",
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.bold),
+                                decoration: InputDecoration(
+                                  enabledBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color:
+                                          Theme.of(context).colorScheme.primary,
+                                    ),
+                                    borderRadius: BorderRadius.circular(6),
+                                  ),
+                                  hintText: '',
+                                  focusedBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color:
+                                          Theme.of(context).colorScheme.primary,
+                                    ),
+                                    borderRadius: BorderRadius.circular(6),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(height: screenSize.height * 0.025),
+                              const Padding(
+                                padding: EdgeInsets.only(bottom: 5),
+                                child: Text.rich(
+                                  textAlign: TextAlign.start,
+                                  TextSpan(
+                                    children: [
+                                      WidgetSpan(
+                                        child: Text(
+                                          'Email',
+                                          style: TextStyle(
+                                              fontSize: 16,
+                                              fontWeight: FontWeight.w300,
+                                              color: Colors.grey),
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              TextFormField(
+                                initialValue: "gnaore.kouame1@gmail.com",
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.bold),
+                                decoration: InputDecoration(
+                                  enabledBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color:
+                                          Theme.of(context).colorScheme.primary,
+                                    ),
+                                    borderRadius: BorderRadius.circular(6),
+                                  ),
+                                  hintText: '',
+                                  focusedBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color:
+                                          Theme.of(context).colorScheme.primary,
+                                    ),
+                                    borderRadius: BorderRadius.circular(6),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(height: screenSize.height * 0.025),
+                              const Padding(
+                                padding: EdgeInsets.only(bottom: 5),
+                                child: Text.rich(
+                                  textAlign: TextAlign.start,
+                                  TextSpan(
+                                    children: [
+                                      WidgetSpan(
+                                        child: Text(
+                                          'Date de naissance',
+                                          style: TextStyle(
+                                              fontSize: 16,
+                                              fontWeight: FontWeight.w300,
+                                              color: Colors.grey),
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              DropdownDatePicker(
+                                locale: "fr_FR",
+                                textStyle: const TextStyle(
+                                    fontWeight: FontWeight.bold),
+                                inputDecoration: InputDecoration(
+                                  enabledBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color:
+                                          Theme.of(context).colorScheme.primary,
+                                    ),
+                                    borderRadius: BorderRadius.circular(6),
+                                  ),
+                                  hintText: 'Ex: Konan',
+                                  focusedBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color:
+                                          Theme.of(context).colorScheme.primary,
+                                    ),
+                                    borderRadius: BorderRadius.circular(6),
+                                  ),
+                                ), // optional
+                                isDropdownHideUnderline: true, // optional
+                                isFormValidator: true, // optional
+                                startYear: 1900, // optional
+                                endYear: DateTime.now().year, // optional
+                                width: 8, // optional
+                                selectedDay: selectedDay, // optional
+                                selectedMonth: selectedMonth, // optional
+                                selectedYear: selectedYear, // optional
+                                onChangedDay: (value) {
+                                  selectedDay = int.parse(value!);
+                                  print('onChangedDay: $value');
+                                },
+                                onChangedMonth: (value) {
+                                  selectedMonth = int.parse(value!);
+                                  print('onChangedMonth: $value');
+                                },
+                                onChangedYear: (value) {
+                                  selectedYear = int.parse(value!);
+                                  print('onChangedYear: $value');
+                                },
+                                // optional
+                                showDay: true, // optional
+                                dayFlex: 2, // optional
+                                hintTextStyle: const TextStyle(
+                                    color: Colors.grey), // optional
+                              ),
+                            ],
+                          ),
+                        ),
+                        Expanded(
+                            child: Center(
+                          child: OutlinedButton(
+                              onPressed: () {},
+                              style: OutlinedButton.styleFrom(
+                                fixedSize: Size(screenSize.width, 50),
+                                side: BorderSide(color: Colors.red),
+                              ),
+                              child: Text(
+                                "Se connecter",
+                                style: TextStyle(color: Colors.red),
+                              )),
+                        ))
                       ],
                     ),
                   ),
